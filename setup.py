a41ea5b9c71e33c4316b9b9d3f1d5343060ef04b
Aadhil <36843795+aadhilpm@users.noreply.github.com>
2018-12-25 00:34:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,21 @@
 from setuptools import setup, find_packages
 import re, ast
 
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+
 # get version from __version__ variable in bdtheme/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('bdtheme/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
-requirements = parse_requirements("requirements.txt", session="")
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 setup(
 	name='bdtheme',
 	version=version,
-	description='bd theme',
+	description='bdtheme',
 	author='vinhbk2000',
 	author_email='vinhbk2000@gmail.com',
 	packages=find_packages(),
